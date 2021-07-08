2b7cda421cb2dee2c0b8339a823a7c948e00886e
guonaihong <guonaihong@qq.com>
2021-07-08 22:04:28 +0800

    frame的解析和序列化

--- frame.go
+++ frame.go
@@ -1,15 +1,23 @@
 package tinyws
 
-import "io"
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+	"math"
+)
 
 const (
 	// 根据5.2描述, 满打满算, 最多14字节
 	maxFrameHeaderSize = 14
 )
 
+var ErrFramePayloadLength = errors.New("error frame payload length")
+
 type frameHeader struct {
-	payloadLen uint64
+	payloadLen int64
 	opcode     int32
+	maskValue  [4]byte
 	rsv1       bool
 	rsv2       bool
 	rsv3       bool
@@ -17,22 +25,112 @@ type frameHeader struct {
 	fin        bool
 }
 
-func (f frameHeader) read(r io.Reader) (h frameHeader, err error) {
+func readHeader(r io.Reader) (h frameHeader, err error) {
 
 	head := make([]byte, 2, maxFrameHeaderSize)
 
-	_, err := io.ReadFull(r, head)
+	_, err = io.ReadFull(r, head)
 	if err != nil {
-		return h, err
+		return
 	}
 
-	h.fin = head[0]&1<<7 > 0
-	h.rsv1 = head[0]&1<<6 > 0
-	h.rsv2 = head[0]&1<<5 > 0
-	h.rsv2 = head[0]&1<<4 > 0
-	h.opcode = head[0] & 0xF
+	h.fin = head[0]&(1<<7) > 0
+	h.rsv1 = head[0]&(1<<6) > 0
+	h.rsv2 = head[0]&(1<<5) > 0
+	h.rsv2 = head[0]&(1<<4) > 0
+	h.opcode = int32(head[0] & 0xF)
 
-	h.mask = head[1]&1<<7 > 0
-	h.payloadLen = head[1] >> 1
+	have := 0
+	h.mask = head[1]&(1<<7) > 0
+	if h.mask {
+		have += 4
+	}
 
+	h.payloadLen = int64(head[1] & 0x7F)
+
+	switch {
+	// 长度
+	case h.payloadLen >= 0 && h.payloadLen <= 125:
+		if h.payloadLen == 0 && !h.mask {
+			return
+		}
+	case h.payloadLen == 126:
+		// 2字节长度
+		have += 2
+	case h.payloadLen == 127:
+		// 8字节长度
+		have += 8
+	default:
+		//预期之外的, 直接报错
+		return h, ErrFramePayloadLength
+	}
+
+	head = head[:have]
+	_, err = io.ReadFull(r, head)
+	if err != nil {
+		return
+	}
+
+	switch h.payloadLen {
+	case 126:
+		h.payloadLen = int64(binary.BigEndian.Uint16(head[:2]))
+		head = head[2:]
+	case 127:
+		h.payloadLen = int64(binary.BigEndian.Uint16(head[:8]))
+		head = head[8:]
+	}
+
+	if h.mask {
+		copy(h.maskValue[:], head)
+	}
+
+	return
+}
+
+// https://datatracker.ietf.org/doc/html/rfc6455#section-5.2
+// (the most significant bit MUST be 0)
+func writeHeader(w io.Writer, h frameHeader) (err error) {
+	head := make([]byte, maxFrameHeaderSize)
+
+	if h.fin {
+		head[0] |= 1 << 7
+	}
+
+	if h.rsv1 {
+		head[0] |= 1 << 6
+	}
+
+	if h.rsv2 {
+		head[0] |= 1 << 5
+	}
+
+	if h.rsv3 {
+		head[0] |= 1 << 5
+	}
+
+	head[0] |= byte(h.opcode & 0xF)
+
+	have := 2
+	switch {
+	case h.payloadLen <= 125:
+		head[1] = byte(h.payloadLen)
+	case h.payloadLen <= math.MaxInt16:
+		head[1] = 126
+		binary.BigEndian.PutUint16(head[2:], uint16(h.payloadLen))
+		have += 2 // 2前
+	case h.payloadLen <= math.MaxInt64:
+		head[1] = 127
+		binary.BigEndian.PutUint64(head[2:], uint64(h.payloadLen))
+		have += 8
+	default:
+		return ErrFramePayloadLength
+	}
+
+	if h.mask {
+		head[1] |= 1 << 7
+		have += copy(head[have:], h.maskValue[:])
+	}
+
+	_, err = w.Write(head[:have])
+	return err
 }
--- frame_test.go
+++ frame_test.go
@@ -1,5 +1,37 @@
 package tinyws
 
-func Test_Frame_Read(t *testing.T) {
-	br := bytes.NewReader([]byte(0x81, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f))
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var noMaskData = []byte{0x81, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f}
+
+func Test_Frame_Read_NoMask(t *testing.T) {
+	r := bytes.NewReader(noMaskData)
+
+	h, err := readHeader(r)
+	assert.NoError(t, err)
+	all, err := io.ReadAll(r)
+	assert.NoError(t, err)
+
+	//fmt.Printf("opcode:%d", h.opcode)
+	assert.Equal(t, string(all), "Hello")
+	assert.Equal(t, h.payloadLen, int64(len("Hello")))
+}
+
+func Test_Frame_Write_NoMask(t *testing.T) {
+	//br := bytes.NewReader([]byte{0x81, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f})
+
+	var w bytes.Buffer
+	var h frameHeader
+	h.payloadLen = int64(5)
+	h.opcode = 1
+	h.fin = true
+	writeHeader(&w, h)
+	w.WriteString("Hello")
+	assert.Equal(t, w.Bytes(), noMaskData)
 }

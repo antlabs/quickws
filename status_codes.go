37426252ab5aa85564a9e7a6d69655a31f63fa16
guonaihong <guonaihong@qq.com>
2021-07-26 22:46:41 +0800

    优化下状态码

--- status_codes.go
+++ status_codes.go
@@ -11,4 +11,16 @@ const (
 	EndpointGoingAway StatusCode = 1001
 	// ProtocolError 表示对端由于协议错误正在终止连接
 	ProtocolError StatusCode = 1002
+	// DataCannotAccept 收到一个不能接受的数据类型
+	DataCannotAccept StatusCode = 1003
+	// NotConsistentMessageType 表示对端正在终止连接, 消息类型不一致
+	NotConsistentMessageType StatusCode = 1007
+	// TerminatingConnection 表示对端正在终止连接, 没有好用的错误, 可以用这个错误码表示
+	TerminatingConnection StatusCode = 1008
+	// TooBigMessage  消息太大, 不能处理, 关闭连接
+	TooBigMessage StatusCode = 1009
+	// NoExtensions 只用于客户端, 服务端返回扩展消息
+	NoExtensions StatusCode = 1010
+	// ServerTerminating 服务端遇到意外情况, 中止请求
+	ServerTerminating StatusCode = 1011
 )

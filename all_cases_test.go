8a9c5d10c6c9d0dea1d05b302556e62a2a828af4
guonaihong <guonaihong@qq.com>
2021-08-09 23:08:34 +0800

    test

---
+++ all_cases_test.go
@@ -0,0 +1,38 @@
+// +build tinywstest
+
+package tinyws
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := Upgrade(w, r)
+	if err != nil {
+		fmt.Println("Upgrade fail:", err)
+		return
+	}
+
+	defer c.Close()
+	for {
+		all, op, err := c.ReadTimeout(3 * time.Second)
+		if err != nil {
+			fmt.Println("err = ", err)
+			return
+		}
+
+		os.Stdout.Write(all)
+		c.WriteTimeout(op, all, 3*time.Second)
+	}
+}
+
+func Test_Server(t *testing.T) {
+	http.HandleFunc("/", echo)
+
+	assert.NoError(t, http.ListenAndServe(":9001", nil))
+}

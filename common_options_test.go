7a5390ae82f1080d2cbbac809369647d28008d18
guonaihong <guonaihong@qq.com>
2023-07-30 23:47:53 +0800

    +测试代码

--- common_options_test.go
+++ common_options_test.go
@@ -124,4 +124,104 @@ func Test_CommonOption(t *testing.T) {
 			t.Error("not run client callback:method fail")
 		}
 	})
+
+	t.Run("server.local: WithServerTCPDelay", func(t *testing.T) {
+		run := int32(0)
+		done := make(chan bool, 1)
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			c, err := Upgrade(w, r, WithServerTCPDelay(), WithServerOnMessageFunc(func(c *Conn, mt Opcode, payload []byte) {
+				atomic.AddInt32(&run, int32(1))
+				done <- true
+			}))
+			if err != nil {
+				t.Error(err)
+			}
+			c.StartReadLoop()
+		}))
+
+		defer ts.Close()
+
+		url := strings.ReplaceAll(ts.URL, "http", "ws")
+		con, err := Dial(url, WithClientCallback(&testDefaultCallback{}))
+		if err != nil {
+			t.Error(err)
+		}
+		defer con.Close()
+
+		con.WriteMessage(Binary, []byte("hello"))
+		select {
+		case <-done:
+		case <-time.After(1000 * time.Millisecond):
+		}
+		if atomic.LoadInt32(&run) != 1 {
+			t.Error("not run server:method fail")
+		}
+	})
+
+	t.Run("server.global: WithServerTCPDelay", func(t *testing.T) {
+		run := int32(0)
+		done := make(chan bool, 1)
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			c, err := Upgrade(w, r, WithServerTCPDelay(), WithServerOnMessageFunc(func(c *Conn, mt Opcode, payload []byte) {
+				atomic.AddInt32(&run, int32(1))
+				done <- true
+			}))
+			if err != nil {
+				t.Error(err)
+			}
+			c.StartReadLoop()
+		}))
+
+		defer ts.Close()
+
+		url := strings.ReplaceAll(ts.URL, "http", "ws")
+		con, err := Dial(url, WithClientCallback(&testDefaultCallback{}))
+		if err != nil {
+			t.Error(err)
+		}
+
+		con.WriteMessage(Binary, []byte("hello"))
+		select {
+		case <-done:
+		case <-time.After(100 * time.Millisecond):
+		}
+		if atomic.LoadInt32(&run) != 1 {
+			t.Error("not run server:method fail")
+		}
+	})
+
+	t.Run("client: WithClientTCPDelay ", func(t *testing.T) {
+		run := int32(0)
+		done := make(chan bool, 1)
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			c, err := Upgrade(w, r, WithServerOnMessageFunc(func(c *Conn, mt Opcode, payload []byte) {
+				c.WriteMessage(mt, payload)
+			}))
+			if err != nil {
+				t.Error(err)
+			}
+			c.StartReadLoop()
+		}))
+
+		defer ts.Close()
+
+		url := strings.ReplaceAll(ts.URL, "http", "ws")
+		con, err := Dial(url, WithClientTCPDelay(), WithClientOnMessageFunc(func(c *Conn, mt Opcode, payload []byte) {
+			atomic.AddInt32(&run, int32(1))
+			done <- true
+		}))
+		if err != nil {
+			t.Error(err)
+		}
+
+		con.StartReadLoop()
+		con.WriteMessage(Binary, []byte("hello"))
+		select {
+		case <-done:
+		case <-time.After(100 * time.Millisecond):
+		}
+		if atomic.LoadInt32(&run) != 1 {
+			t.Error("not run client callback:method fail")
+		}
+	})
 }
